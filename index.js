634bc539d01eb07e126b9f33eed999f84b6d626e
carolina-ross <97426898+carolina-ross@users.noreply.github.com>
2022-07-03 22:13:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Your code here
+
+var questionsArr = [
+    {
+    question: 'The Big Apple is a nickname given to wich city?',  //0
+    answer: 'New York',
+    options: [
+        'San Francisco',
+        'New York',
+        'Los Angeles',
+        'Miami',
+    ]
+    },
+
+    {
+    question: 'Who discovered Penicillin?',  //1
+    answer: 'Alexander Fleming',
+    options: [
+        'Alexander Fleming',
+        'Marie Curie',
+        'Louis Pasteur',
+        'Karl Landsteiner',
+    ]   
+    },
+
+    {
+    question: 'What is the largest river in the world?',  //2
+    answer: 'Nile',
+    options: [
+        'Amazon',
+        'Mississippi',
+        'Volga',
+        'Nile',
+    ] 
+    },
+
+    {
+    question: 'Tesla is most often remembered for his pioneering work and innovations with regard to which electrical technology?',  //3
+    answer: 'Alternating Current',
+    options: [
+        'Direct Current',
+        'Indirect Current',
+        'Alternating Current',
+        'Nominal Current',
+    ] 
+    },
+
+    {
+    question: 'HTML is a...?',  //4
+    answer: 'Markup Language',
+    options: [
+        'Machine Language',
+        'Markup Language',
+        'Programming Language',
+        'Script Language',
+    ] 
+    }
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
